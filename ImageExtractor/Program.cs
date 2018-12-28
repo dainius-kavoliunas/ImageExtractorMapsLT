7009def02bc4bf15d8bae8be8e7558ccfc2059e9
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-12-28 10:06:12 +0200

    FirstCommit

--- ImageExtractor/Program.cs
+++ ImageExtractor/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,40 @@ namespace ImageExtractor
     {
         static void Main(string[] args)
         {
+
+            //double minX = 10129, minY = 18291, maxX = 10537, maxY = 18826;
+            //double minX = 5064, minY = 9145, maxX = 5268, maxY = 9413, scale = 14;
+            //double minX = 316, minY = 571, maxX = 329, maxY = 588, scale = 10;
+            //double minX = 1266, minY = 2286, maxX = 1317, maxY = 2353, scale = 12;
+
+            //double minX = 1285, minY = 2310, maxX = 1317, maxY = 2353, scale = 12;
+            //int minX = 10191, minY = 18685, maxX = 10206, maxY = 18699, scale = 15;
+            //double minX = 81544, minY = 149539, maxX = 81669, maxY = 149593, scale = 18;
+            double minX = 20382, minY = 37383, maxX = 20420, maxY = 37401, scale = 16;
+
+            var url =
+                "https://beta.maps.lt/arcgis-maps/rest/services/Basemaps/maps_ortofoto_wmerc/MapServer/tile/{0}/{1}/{2}";
+
+
+            if (!Directory.Exists($"{scale}")) Directory.CreateDirectory($"{scale}");
+
+            for (var y = minY; y <= maxY; y++)
+            {
+
+                Console.WriteLine((y - minY) / (maxY - minY) * 100);
+
+
+                for (var x = minX; x <= maxX; x++)
+                {
+                    var newUrl = string.Format(url, scale, x, y);
+
+                    using (WebClient client = new WebClient())
+                    {
+                        client.DownloadFile(newUrl, $"{scale}/{x}.{y}.jpg");
+                    }
+
+                }
+            }
         }
     }
 }
---
+++ ImageMerger/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+
+namespace ImageMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int minX = 316, minY = 571, maxX = 329, maxY = 588, scale = 10;
+            //int minX = 10191, minY = 18685, maxX = 10206, maxY = 18699, scale = 15;
+            //double minX = 81544, minY = 149539, maxX = 81669, maxY = 149593, scale = 18;
+            double minX = 20382, minY = 37383, maxX = 20420, maxY = 37401, scale = 16;
+
+
+            if (!Directory.Exists($"{scale}")) Directory.CreateDirectory($"{scale}");
+
+            for (var x = minX; x <= maxX; x++)
+            {
+                Bitmap bitmap = new Bitmap(width: (int)(256 * (maxY - minY + 1)), height: 256);
+
+
+                for (var y = minY; y <= maxY; y++)
+                {
+
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+
+                        Image img = Image.FromFile(
+                            $"C:\\Users\\daini\\Desktop\\ImageExtractor\\ImageExtractor\\bin\\Debug\\{scale}\\{x}.{y}.jpg");
+
+                        g.DrawImage(img, (int)(y - minY) * 256, 0);
+
+                        img.Dispose();
+                    }
+                }
+
+                bitmap.Save($"{scale}\\" + x + "result.jpg");
+            }
+
+            Bitmap bitmap2 = new Bitmap(width: 256 * (int)(maxY - minY + 1), height: (int)(maxX - minX + 1) * 256);
+
+            using (Graphics g = Graphics.FromImage(bitmap2))
+            {
+
+                for (var x = minX; x <= maxX; x++)
+                {
+                Console.WriteLine((x - minX) / (maxX - minX) * 100);
+
+                    Image img = Image.FromFile($"{scale}\\" + x + "result.jpg");
+
+                    g.DrawImage(img, 0, (int)(x-minX) * 256 );
+
+                    img.Dispose();
+                }
+            }
+
+
+
+            bitmap2.Save($"{scale}\\{scale}.jpg");
+
+        }
+    }
+}
